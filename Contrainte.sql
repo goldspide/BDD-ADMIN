59a4dffc05ae84627a7c5167e233ddb5e64f1765
RomiUber <lorisdassiuber@gmail.com>
2022-11-18 19:19:22 +0100

    ajout des contraintes

--- Contrainte.sql
+++ Contrainte.sql
@@ -160,3 +160,70 @@ ADD (
                      REFERENCES PRESSING (idPressing)
     ) ;
     commit;
+
+-- contrainte de la table PROMO
+  ALTER TABLE PROMO 
+  ADD (
+
+    CONSTRAINT      OFFRESERVICEidServie_fk
+                     FOREIGN KEY (SERVICEidService)
+                     REFERENCES SERVICE (idService)
+
+    , CONSTRAINT    OFRRETYPE_LINGEidType_L_fk
+                     FOREIGN KEY (TYPE_LINGEidType_L)
+                     REFERENCES TYPE_LINGE (idType_L)  
+
+
+     , CONSTRAINT    ANNONCEidAnnonce_fk
+                     FOREIGN KEY (ANNONCEidAnnonce)
+                     REFERENCES ANNONCE (idAnnonce) 
+
+                     ) ;
+    commit;
+
+
+
+-- contrainte de la table PRIX
+   ALTER TABLE PRIX
+  ADD (
+
+     CONSTRAINT      prix_pk
+                     PRIMARY KEY (Tarif_U)
+    , 
+
+     CONSTRAINT      PROMOOFFRETYPE_LINGEidType_L 
+                     FOREIGN KEY (PROMOOFFRETYPE_LINGEidType_L )
+                     REFERENCES TYPE_LINGE (idType_L )
+
+    , CONSTRAINT    PROMOOFFRESERVICEidService 
+                     FOREIGN KEY (PROMOOFFRESERVICEidService )
+                     REFERENCES SERVICE (idService )  
+
+     CONSTRAINT      PROMOANNONCEidAnnonce
+                     FOREIGN KEY (PROMOANNONCEidAnnonce)
+                     REFERENCES ANNONCE (idAnnonce)
+
+    , CONSTRAINT    LINGECLIENTTYPE_LINGEidType_L 
+                     FOREIGN KEY (LINGECLIENTTYPE_LINGEidType_L )
+                     REFERENCES TYPE_LINGE (idType_L )  
+
+
+     , CONSTRAINT    LINGECLIENTTYPE_LINGEidType_L 
+                     FOREIGN KEY (LINGECLIENTBESOINCLIENTidBesoinClient )
+                     REFERENCES BESOINCLIENT (idBesoinClient )  
+
+ 
+                  ) ;
+    
+commit;
+
+  
+    
+    
+     
+    
+    
+    
+  
+ 
+
