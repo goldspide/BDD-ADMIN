e59292d77c63a3a9710f8693fa346f93c8e6a42f
goldspide <raoulb69@gmal.com>
2022-12-06 18:57:56 +0100

    amelioration

--- Gestion_Pressing/gp_fonction/count.sql
+++ Gestion_Pressing/gp_fonction/count.sql
@@ -1,6 +1,6 @@
 DECLARE
 counte number := 0;
 BEGIN
-select count(*) from Utilisateur into counte;
+select count(*) into counte from Utilisateur;
 DBMS_OUTPUT.PUT_LINE('Le nombre de valeur presente est'|| ':'|| counte);
 END;
