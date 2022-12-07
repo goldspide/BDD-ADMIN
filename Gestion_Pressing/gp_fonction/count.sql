72ec7554116f36b78035c43bd2176a7f09ff0aa3
goldspide <raoulbonsso@gmal.com>
2022-12-07 18:34:22 +0100

    creation suplementaire

--- Gestion_Pressing/gp_fonction/count.sql
+++ Gestion_Pressing/gp_fonction/count.sql
@@ -3,6 +3,6 @@ counte number := 0;
 BEGIN
 select count(*) into counte from Utilisateur;
 DBMS_OUTPUT.PUT_LINE('Le nombre de valeur presente est'|| ':'|| counte);
-END;  
+END;   
 
 
--- Gestion_Pressing/gp_table/Utilisateur.sql
+++ Gestion_Pressing/gp_table/Utilisateur.sql
@@ -1,7 +1,7 @@
  Create table UTILISATEUR(
     idUtilisateur VARCHAR2(30) PRIMARY KEY NOT NULL,
-    Nom_Utilisateur VARCHAR2(30) NOT NULL,
-    Tel_Utilisateur VARCHAR2(30) NOT NULL
+    login VARCHAR2(30) NOT NULL,
+    password VARCHAR2(30) NOT NULL
 
  );
 
