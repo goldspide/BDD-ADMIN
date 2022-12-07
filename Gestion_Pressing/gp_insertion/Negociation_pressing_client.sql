8bfe1c70081cfbdb2bda994bc82fe17f3d9e08b6
goldspide <raoulbonsso@gmal.com>
2022-12-07 19:02:54 +0100

    amelioration

--- Gestion_Pressing/gp_insertion/Negociation_pressing_client.sql
+++ Gestion_Pressing/gp_insertion/Negociation_pressing_client.sql
@@ -1,17 +1,61 @@
-CREATE SEQUENCE id_pressing
-MINVALUE 1
-START WITH 1
-INCREMENT BY 1
-CACHE 50;
-
-CREATE SEQUENCE id_besoin
-MINVALUE 1
-START WITH 1
-INCREMENT BY 1
-CACHE 50;
+INSERT INTO NEGOCIATION_PRESSING_CLIENT
+VALUES(
+    id_nego.nextval,
+    idpressing.nextval,
+    id_besoin.nextval
+);
 
 INSERT INTO NEGOCIATION_PRESSING_CLIENT
 VALUES(
+    id_nego.nextval,
+    idpressing.nextval,
+    id_besoin.nextval
+);
+
+INSERT INTO NEGOCIATION_PRESSING_CLIENT
+VALUES(
+    id_nego.nextval,
+    idpressing.nextval,
+    id_besoin.nextval
+);
+
+INSERT INTO NEGOCIATION_PRESSING_CLIENT
+VALUES(
+    id_nego.nextval,
+    idpressing.nextval,
+    id_besoin.nextval
+);
+
+INSERT INTO NEGOCIATION_PRESSING_CLIENT
+VALUES(
+    id_nego.nextval,
+    idpressing.nextval,
+    id_besoin.nextval
+);
+
+INSERT INTO NEGOCIATION_PRESSING_CLIENT
+VALUES(
+    id_nego.nextval,
+    idpressing.nextval,
+    id_besoin.nextval
+);
+
+INSERT INTO NEGOCIATION_PRESSING_CLIENT
+VALUES(
+    idpressing.nextval,
+    id_besoin.nextval
+);
+
+INSERT INTO NEGOCIATION_PRESSING_CLIENT
+VALUES(
+    id_nego.nextval,
+    idpressing.nextval,
+    id_besoin.nextval
+);
+
+INSERT INTO NEGOCIATION_PRESSING_CLIENT
+VALUES(
+    id_nego.nextval,
     idpressing.nextval,
     id_besoin.nextval
 );
@@ -30,66 +74,21 @@ VALUES(
 
 INSERT INTO NEGOCIATION_PRESSING_CLIENT
 VALUES(
+    id_nego.nextval,
     idpressing.nextval,
     id_besoin.nextval
 );
 
 INSERT INTO NEGOCIATION_PRESSING_CLIENT
 VALUES(
+    id_nego.nextval,
     idpressing.nextval,
     id_besoin.nextval
 );
 
 INSERT INTO NEGOCIATION_PRESSING_CLIENT
 VALUES(
-    idpressing.nextval,
-    id_besoin.nextval
-);
-
-INSERT INTO NEGOCIATION_PRESSING_CLIENT
-VALUES(
-    idpressing.nextval,
-    id_besoin.nextval
-);
-
-INSERT INTO NEGOCIATION_PRESSING_CLIENT
-VALUES(
-    idpressing.nextval,
-    id_besoin.nextval
-);
-
-INSERT INTO NEGOCIATION_PRESSING_CLIENT
-VALUES(
-    idpressing.nextval,
-    id_besoin.nextval
-);
-
-INSERT INTO NEGOCIATION_PRESSING_CLIENT
-VALUES(
-    idpressing.nextval,
-    id_besoin.nextval
-);
-
-INSERT INTO NEGOCIATION_PRESSING_CLIENT
-VALUES(
-    idpressing.nextval,
-    id_besoin.nextval
-);
-
-INSERT INTO NEGOCIATION_PRESSING_CLIENT
-VALUES(
-    idpressing.nextval,
-    id_besoin.nextval
-);
-
-INSERT INTO NEGOCIATION_PRESSING_CLIENT
-VALUES(
-    idpressing.nextval,
-    id_besoin.nextval
-);
-
-INSERT INTO NEGOCIATION_PRESSING_CLIENT
-VALUES(
+    id_nego.nextval,
     idpressing.nextval,
     id_besoin.nextval
 );
